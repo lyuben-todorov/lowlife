40fe9bd0e188b446a192ca126e2056e8d034a222
DESKTOP-2H30L6F\Lyuben Todorov <lyuben.todorov@gmail.com>
2018-12-13 02:13:34 +0200

    Commit for testing lazyload in production

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+
+var el = document.querySelector('img');
+var observer = lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
+observer.observe();
