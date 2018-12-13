11526511a8189ff51277a79965906fb4cab1535a
DESKTOP-2H30L6F\Lyuben Todorov <lyuben.todorov@gmail.com>
2018-12-13 13:27:25 +0200

    Finished about modal styles and backdrop; media icons tbd

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     $(this).ekkoLightbox();
 });
 
+$('#about').on('click', function(e){
+    $('#aboutModal').modal();
+    $('div.modal-backdrop').addClass("about-backdrop");
+
+});
 var el = document.querySelector('img');
 var observer = lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
 observer.observe();
